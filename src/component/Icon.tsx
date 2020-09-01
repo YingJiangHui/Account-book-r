3c2b9ca0c78c204ca5ddbd4853dd5c8e491ff6e9
dark <473380917@qq.com>
2020-09-01 15:51:35 +0800

    下载classnams库对react中className进行处理
    对Icon组件优化

--- src/component/Icon.tsx
+++ src/component/Icon.tsx
@@ -1,16 +1,18 @@
-import React from 'react'
+import React from 'react';
+import cs from 'classnames';
 
 const importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
 try {importAll(require.context('icons', true, /\.svg$/));} catch (e) {console.log(e);}
 
 type Props = {
-  name:string
-}
-const Icon=(props:Props)=>{
+  name?: string
+} & React.SVGAttributes<SVGElement>
+const Icon = (props: Props) => {
+  const {className, ...rest} = props;
   return (
-    <svg className='icon'>
-      <use xlinkHref={'#'+props.name}></use>
+    <svg className={cs('icon',className)}>
+      {rest.name ? <use xlinkHref={'#' + rest.name}></use> : ''}
     </svg>
-  )
-}
-export default Icon
+  );
+};
+export default Icon;
