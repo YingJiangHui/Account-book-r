2314cc4c70b67b858230885ea82c4ab38d37f8a0
dark <473380917@qq.com>
2020-09-29 21:36:10 +0800

    修改弹窗样式

--- src/component/PopUp/alertSelectBox/AlertSelectBox.tsx
+++ src/component/PopUp/alertSelectBox/AlertSelectBox.tsx
@@ -1,8 +1,10 @@
-import React, {FC, memo, useEffect, useState} from 'react';
+import React, {FC, memo, useEffect, useRef, useState} from 'react';
 import styled from 'styled-components';
-import Cover from './popUpBoxComponent/popUpRootComponent/Cover';
+import Cover from '../popUpBoxComponent/popUpRootComponent/Cover';
 import {CSSTransition} from 'react-transition-group';
-import HintBox from './HintBox';
+import HintBox from '../HintBox';
+import 'style/animation.scss';
+
 
 const Wrapper = styled.div`
   background: #fff;
@@ -12,7 +14,6 @@ const Wrapper = styled.div`
   left: 50%;
   transform: translate(-50%,-50%);
   width: 80%;
-  height: 220px;
 `;
 const View = styled.div`
   display: flex;
@@ -20,7 +21,7 @@ const View = styled.div`
   align-items: center;
   border-bottom: 1px solid rgba(0,0,0,0.1);
   width: 100%;
-  height: 70%;
+  padding: 60px 0;
 `;
 const Control = styled.div`
   height: 30%;
@@ -28,10 +29,11 @@ const Control = styled.div`
 
     background: transparent;
     width: 50%;    
-    height: 100%;
+    padding: 20px 0;
     border: none;
         border-right: 1px solid rgba(0,0,0,0.1);
    &:last-child{
+        color: #db4460;
         border-right:none;
   }
   }
@@ -41,41 +43,43 @@ type Props = {
   ensure: () => void
   cancel: () => void
   show: boolean
-  beforeTip?:string
+  beforeTip?: string
 }
-const AlertSelectBox: FC<Props> = memo(({value, children, ensure, cancel, show,beforeTip}) => {
+const AlertSelectBox: FC<Props> = memo(({value, children, ensure, cancel, show, beforeTip}) => {
   const [visible, setVisible] = useState(false);
   const [hintVisible, setHintVisible] = useState(false);
-
   useEffect(() => {
     setVisible(show);
-  }, [show]);
+    }, [show]);
+  useEffect(()=>{
+    console.log(hintVisible)
+  },[hintVisible])
   return (
     <>
-    <CSSTransition timeout={200} classNames={'fade'} in={visible} unmountOnExit={true}>
-      <Cover>
+      <Cover show={visible}/>
+      <CSSTransition timeout={250} classNames={'fade'} className={'fade'} in={visible} unmountOnExit={true}>
         <Wrapper>
           <View>{value}</View>
           <Control>
-            <button onClick={
-              () => {
-                ensure();
-                setHintVisible(true)
-              }}>确定
-            </button>
             <button onClick={() => {
               cancel();
             }}>取消
             </button>
+            <button onClick={
+              () => {
+                ensure();
+                setHintVisible(true);
+              }}>确定
+            </button>
+
           </Control>
         </Wrapper>
-      </Cover>
-    </CSSTransition>
-      {beforeTip?
-        <HintBox text={beforeTip} show={hintVisible} onChange={(value:boolean)=>{setHintVisible(value)}}/>
-      :''}
+      </CSSTransition>
+      {beforeTip ?
+        <HintBox text={beforeTip} show={hintVisible} onChange={(value: boolean) => {setHintVisible(value);}}/>
+        : ''}
 
-      </>
+    </>
   );
 });
 
